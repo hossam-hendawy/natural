38d9d69379b8edf23d9dd60905ea223f672847df
Hossam <hossam@digihive.dev>
2025-02-05 19:55:09 +0200

    Feat : init home here block

---
+++ wp-content/themes/natural/front-end/src/blocks/home_here_block/index.js
@@ -0,0 +1,8 @@
+import './style.scss';
+
+export function home_here_block() {
+  const footerSelector = document.querySelector('.home_here_block');
+  if (!footerSelector) return;
+  
+}
+
--- wp-content/themes/natural/front-end/src/blocks/index-blocks.js
+++ wp-content/themes/natural/front-end/src/blocks/index-blocks.js
@@ -2,10 +2,12 @@
 import {headerBlock} from "./headerBlock";
 import {footerBlock} from './footerBlock';
 import {image_block} from './image_block';
+import {home_here_block} from './home_here_block';
 
 export function indexBlocks() {
   headerBlock()
   footerBlock()
   image_block()
+  home_here_block()
 }
 
