62b86f6ff9c6b6f2a9566cce149c95146cd3b147
Hossam <hossam@digihive.dev>
2025-02-07 17:03:47 +0200

    Feat: working what_we_offer_block

--- wp-content/themes/natural/blocks/what_we_offer_block/index.php
+++ wp-content/themes/natural/blocks/what_we_offer_block/index.php
@@ -36,8 +36,111 @@ $cta_button = get_field('cta_button');
           <h2 class="label light fz-14 fw-400"><?= $label ?></h2>
         <?php } ?>
         <?php if ($title) { ?>
-          <h2 class="title natural-h2 fw-400 lh-72"><?= $title ?></h2>
+          <h2 class="main-title natural-h2 fw-400 lh-72"><?= $title ?></h2>
         <?php } ?>
+        
+        <div class="accordion">
+          <div class="accordion-panel active" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+            <div id="panel2-title" class="accordion-title">
+              <button class="accordion-trigger medium" aria-expanded="true">
+                <span>Chargrilling</span>
+                <span class="toggle-open minus-plus">
+             <svg width="50" height="50" viewBox="0 0 50 50" fill="none" aria-hidden="true">
+          <line class="vertical-line" x1="25" y1="5" x2="25" y2="45" stroke="#98A2B3" stroke-width="5"></line>
+          <line class="horizontal-line" x1="5" y1="25" x2="45" y2="25" stroke="#98A2B3" stroke-width="5"></line>
+        </svg>
+               </span>
+              </button>
+            </div>
+            <div class="accordion-content" role="region" aria-labelledby="panel2-title" aria-hidden="false">
+              <div class="answer" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                <p class="spacer"></p>
+                <p class="description">Flame grilling hearty veg to bring the smoky BBQ flavours consumers can’t get enough of.</p>
+                <a class="cta-link fz-16 spinach-color  fw-400" href="http://localhost/natural/2025/02/06/the-importance-of-shelf-life-dates-3/" target="_self">
+                  Read more
+                  <svg width="15" height="12" viewBox="0 0 15 12" fill="none" aria-hidden="true">
+                    <path d="M11.0332 4.18271e-07L8.59375 0C8.59375 2.4252 9.50345 3.79726 10.476 4.57086C11.287 5.21576 12.2971 5.55899 13.3333 5.55899C12.2971 5.55899 11.287 5.90178 10.476 6.54712C9.50301 7.32072 8.59375 8.69234 8.59375 11.118L11.0332 11.118C11.0332 11.118 10.7448 6.62143 14.5743 5.55943C10.7448 4.49656 11.0332 4.18271e-07 11.0332 4.18271e-07Z" fill="#1C301A"></path>
+                    <path d="M9.66851 5.55855C5.83344 4.49656 6.12221 1.04972e-06 6.12221 1.04972e-06L3.67917 6.30837e-07C3.67917 2.4252 4.59022 3.79726 5.56415 4.57086C5.97118 4.8942 6.42825 5.14145 6.91367 5.30776L-7.06768e-07 3.96401L-1.27553e-06 7.15397L6.91367 5.81023C6.42825 5.97654 5.97162 6.22379 5.5646 6.54712C4.59022 7.32072 3.67961 8.69234 3.67961 11.118L6.12265 11.118C6.12265 11.118 5.83388 6.62143 9.66895 5.55943" fill="#1C301A"></path>
+                  </svg>
+                </a>
+                
+              </div>
+            </div>
+          </div>
+          <div class="accordion-panel" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+            <div id="panel2-title" class="accordion-title">
+              <button class="accordion-trigger medium" aria-expanded="false">
+                <span>Roasting</span>
+                <span class="toggle-open minus-plus">
+             <svg width="50" height="50" viewBox="0 0 50 50" fill="none" aria-hidden="true">
+          <line class="vertical-line" x1="25" y1="5" x2="25" y2="45" stroke="#98A2B3" stroke-width="5"></line>
+          <line class="horizontal-line" x1="5" y1="25" x2="45" y2="25" stroke="#98A2B3" stroke-width="5"></line>
+        </svg>
+        </span>
+              </button>
+            </div>
+            <div class="accordion-content" role="region" aria-labelledby="panel2-title">
+              <div class="answer" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                <p class="spacer"></p>
+                <p class="description">Flame grilling hearty veg to bring the smoky BBQ flavours consumers can’t get enough of.</p>
+                <a class="cta-link fz-16 spinach-color  fw-400" href="http://localhost/natural/2025/02/06/the-importance-of-shelf-life-dates-3/" target="_self">
+                  Read more
+                  <svg width="15" height="12" viewBox="0 0 15 12" fill="none" aria-hidden="true">
+                    <path d="M11.0332 4.18271e-07L8.59375 0C8.59375 2.4252 9.50345 3.79726 10.476 4.57086C11.287 5.21576 12.2971 5.55899 13.3333 5.55899C12.2971 5.55899 11.287 5.90178 10.476 6.54712C9.50301 7.32072 8.59375 8.69234 8.59375 11.118L11.0332 11.118C11.0332 11.118 10.7448 6.62143 14.5743 5.55943C10.7448 4.49656 11.0332 4.18271e-07 11.0332 4.18271e-07Z" fill="#1C301A"></path>
+                    <path d="M9.66851 5.55855C5.83344 4.49656 6.12221 1.04972e-06 6.12221 1.04972e-06L3.67917 6.30837e-07C3.67917 2.4252 4.59022 3.79726 5.56415 4.57086C5.97118 4.8942 6.42825 5.14145 6.91367 5.30776L-7.06768e-07 3.96401L-1.27553e-06 7.15397L6.91367 5.81023C6.42825 5.97654 5.97162 6.22379 5.5646 6.54712C4.59022 7.32072 3.67961 8.69234 3.67961 11.118L6.12265 11.118C6.12265 11.118 5.83388 6.62143 9.66895 5.55943" fill="#1C301A"></path>
+                  </svg>
+                </a>
+
+              </div>
+            </div>
+          </div>
+          <div class="accordion-panel" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+            <div id="panel2-title" class="accordion-title">
+              <button class="accordion-trigger medium" aria-expanded="false">
+                <span>Blanching</span>
+                <span class="toggle-open minus-plus">
+             <svg width="50" height="50" viewBox="0 0 50 50" fill="none" aria-hidden="true">
+          <line class="vertical-line" x1="25" y1="5" x2="25" y2="45" stroke="#98A2B3" stroke-width="5"></line>
+          <line class="horizontal-line" x1="5" y1="25" x2="45" y2="25" stroke="#98A2B3" stroke-width="5"></line>
+        </svg>
+        </span>
+              </button>
+            </div>
+            <div class="accordion-content" role="region" aria-labelledby="panel2-title">
+              <div class="answer" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                <p class="spacer"></p>
+                <p class="description">Flame grilling hearty veg to bring the smoky BBQ flavours consumers can’t get enough of.</p>
+                <a class="cta-link fz-16 spinach-color  fw-400" href="http://localhost/natural/2025/02/06/the-importance-of-shelf-life-dates-3/" target="_self">
+                  Read more
+                  <svg width="15" height="12" viewBox="0 0 15 12" fill="none" aria-hidden="true">
+                    <path d="M11.0332 4.18271e-07L8.59375 0C8.59375 2.4252 9.50345 3.79726 10.476 4.57086C11.287 5.21576 12.2971 5.55899 13.3333 5.55899C12.2971 5.55899 11.287 5.90178 10.476 6.54712C9.50301 7.32072 8.59375 8.69234 8.59375 11.118L11.0332 11.118C11.0332 11.118 10.7448 6.62143 14.5743 5.55943C10.7448 4.49656 11.0332 4.18271e-07 11.0332 4.18271e-07Z" fill="#1C301A"></path>
+                    <path d="M9.66851 5.55855C5.83344 4.49656 6.12221 1.04972e-06 6.12221 1.04972e-06L3.67917 6.30837e-07C3.67917 2.4252 4.59022 3.79726 5.56415 4.57086C5.97118 4.8942 6.42825 5.14145 6.91367 5.30776L-7.06768e-07 3.96401L-1.27553e-06 7.15397L6.91367 5.81023C6.42825 5.97654 5.97162 6.22379 5.5646 6.54712C4.59022 7.32072 3.67961 8.69234 3.67961 11.118L6.12265 11.118C6.12265 11.118 5.83388 6.62143 9.66895 5.55943" fill="#1C301A"></path>
+                  </svg>
+                </a>
+
+              </div>
+            </div>
+          </div>
+          <div class="accordion-panel" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+            <div id="panel2-title" class="accordion-title">
+              <button class="accordion-trigger medium" aria-expanded="false">
+                <span>Washing and Prepping</span>
+                <span class="toggle-open minus-plus">
+             <svg width="50" height="50" viewBox="0 0 50 50" fill="none" aria-hidden="true">
+          <line class="vertical-line" x1="25" y1="5" x2="25" y2="45" stroke="#98A2B3" stroke-width="5"></line>
+          <line class="horizontal-line" x1="5" y1="25" x2="45" y2="25" stroke="#98A2B3" stroke-width="5"></line>
+        </svg>
+        </span>
+              </button>
+            </div>
+            <div class="accordion-content" role="region" aria-labelledby="panel2-title">
+              <div class="answer" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                <p class="spacer"></p>
+                <p>Flame grilling hearty veg to bring the smoky BBQ flavours consumers can’t get enough of.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- wp-content/themes/natural/front-end/src/blocks/what_we_offer_block/index.js
+++ wp-content/themes/natural/front-end/src/blocks/what_we_offer_block/index.js
@@ -1,8 +1,36 @@
 import './style.scss';
 
 export function what_we_offer_block() {
-  const footerSelector = document.querySelector('.what_we_offer_block');
-  if (!footerSelector) return;
+  const blockSelector = document.querySelector('.what_we_offer_block');
+  if (!blockSelector) return;
   
-}
+  const accordion = blockSelector.querySelector(".accordion");
+  
 
+  
+  accordion.addEventListener("click", (e) => {
+    const activePanel = e.target.closest(".accordion-panel");
+    if (!activePanel) return;
+    toggleAccordion(activePanel);
+  });
+  
+  function toggleAccordion(panelToActivate, forceOpen = false) {
+    const activeButton = panelToActivate.querySelector("button");
+    const activePanel = panelToActivate.querySelector(".accordion-content");
+    const activePanelIsOpened = activeButton.getAttribute("aria-expanded") === "true";
+    
+    // Remove active class from all panels
+    document.querySelectorAll(".accordion-panel").forEach(panel => {
+      panel.classList.remove("active");
+      panel.querySelector("button").setAttribute("aria-expanded", false);
+      panel.querySelector(".accordion-content").setAttribute("aria-hidden", true);
+    });
+    
+    // Toggle current panel
+    if (!activePanelIsOpened || forceOpen) {
+      activeButton.setAttribute("aria-expanded", true);
+      activePanel.setAttribute("aria-hidden", false);
+      panelToActivate.classList.add("active");
+    }
+  }
+}
