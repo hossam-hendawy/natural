bb84817b3a4bac7270b9264c0c813a08026527f9
Hossam <hossam@digihive.dev>
2025-02-08 16:40:04 +0200

    Feat: working in Text Slider back end done

--- wp-content/themes/natural/blocks/home_here_block/index.php
+++ wp-content/themes/natural/blocks/home_here_block/index.php
@@ -21,11 +21,15 @@ if (isset($block)) {
 }
 ?>
 <?php
+$title = get_field('title');
+$description = get_field('description');
+$cta_button = get_field('cta_button');
 $media_image = get_field('image');
 $video = get_field('video');
 ?>
 <section id="<?= esc_attr($id) ?>" class="<?= esc_attr($className) ?>">
   <div class="container">
+    
     <?php if ($select_media = get_field('select_media') === 'video') { ?>
       <div class="aspect-ratio media-wrapper">
         <video playsinline autoplay muted loop src="<?= $video ?>" class="video " data-video-type="video_file"></video>
@@ -52,5 +56,48 @@ $video = get_field('video');
         ?>
       </div>
     <?php } ?>
+    
+    
+    <div class="content-wrapper">
+      <svg class="corner trc" width="7" height="7" viewBox="0 0 7 7" fill="none">
+        <path opacity="0.5" d="M0.212232 1C3.40657 1 5.99609 3.58952 5.99609 6.78386" stroke="#FFFDE7"
+              stroke-width="0.827039"/>
+      </svg>
+      <svg class="corner tlc" width="7" height="7" viewBox="0 0 7 7" fill="none">
+        <path opacity="0.5" d="M1 6.78386C1 3.58952 3.58952 1 6.78386 1" stroke="#FFFDE7" stroke-width="0.827039"/>
+      </svg>
+      <svg class="corner brc" width="7" height="7" viewBox="0 0 7 7" fill="none">
+        <path opacity="0.5" d="M5.99609 0.216138C5.99609 3.41048 3.40657 6 0.21223 6" stroke="#FFFDE7" stroke-width="0.827039"/>
+      </svg>
+      <svg class="corner blc" width="7" height="7" viewBox="0 0 7 7" fill="none">
+        <path opacity="0.5" d="M6.78386 6C3.58952 6 1 3.41048 1 0.216137" stroke="#FFFDE7" stroke-width="0.827039"/>
+      </svg>
+      <svg class="corner tcc" width="13" height="7" viewBox="0 0 13 7" fill="none">
+        <path d="M6.25293 6.78386C6.25293 3.58952 8.84245 1 12.0368 1" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+        <path d="M0.46907 0.999998C3.66341 0.999999 6.25293 3.58952 6.25293 6.78386" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+      </svg>
+      <svg class="corner bcc" width="12" height="7" viewBox="0 0 12 7" fill="none">
+        <path d="M6.14502 7.50124e-05C6.14502 3.19441 3.5555 5.78394 0.361158 5.78394" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+        <path d="M11.9289 5.78394C8.73454 5.78394 6.14502 3.19441 6.14502 7.4029e-05" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+      </svg>
+      <svg class="corner mrc" width="7" height="12" viewBox="0 0 7 12" fill="none">
+        <path d="M0.208326 5.78369C3.40266 5.78369 5.99219 8.37321 5.99219 11.5676" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+        <path d="M5.99219 -0.00016962C5.99219 3.19417 3.40266 5.78369 0.208325 5.78369" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+      </svg>
+      <svg class="corner mlc" width="7" height="12" viewBox="0 0 7 12" fill="none">
+        <path d="M6.78386 5.78394C3.58952 5.78394 1 3.19441 1 7.35372e-05" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+        <path d="M0.999999 11.5678C1 8.37346 3.58952 5.78394 6.78386 5.78394" stroke="#FFFDE7" stroke-width="0.827039" opacity="0.5"/>
+      </svg>
+      <?php if ($title): ?>
+        <h1 class="natural-h1 main-title"><?= $title ?></h1>
+      <?php endif; ?>
+      <?php if ($description): ?>
+        <div class="fz-24 description fw-200"><?= $description ?></div>
+      <?php endif; ?>
+      <?php if (!empty($cta_button) && is_array($cta_button)) { ?>
+        <a class="cta-button" href="<?= $cta_button['url'] ?>" target="<?= $cta_button['target'] ?>"><?= $cta_button['title'] ?></a>
+      <?php } ?>
+    
+    </div>
   </div>
 </section>
